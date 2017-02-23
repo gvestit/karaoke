8d0676ccb31477a103d9f9d869ed8bb1a4111390
gerardgarrote <gerardgarrote@gmail.com>
2017-02-23 11:14:54 -0500

    message

--- js/motor.js
+++ js/motor.js
@@ -1,56 +1,173 @@
- var canso = [{
-   lletra: 'Test prova 1, 2, 3',
-   temps: 1,
+//NIVELL DIFICULTAT, 2 EMINEM 
+var canso = [{
+   lletra: '¡Ay!',
+   temps: 47,
    tempsPintar: 1
- }, {
-   lletra: 'lorem impas dweiwjfiqd',
-   temps: 2,
+ }, 
+ {
+   lletra: '¡Fonsi!',
+   temps: 47,
    tempsPintar: 1
- }, {
-   lletra: 'asdmwemf we fjaisjd iqwjd',
-   temps: 3,
+ },
+{
+   lletra: '¡DY!',
+   temps: 47,
    tempsPintar: 1
- }, {
-   lletra: 'asdmwemf we fjaisjd iqwjd',
-   temps: 4,
+ },  
+ {
+   lletra: '¡Oh oh...!',
+   temps: 47,
    tempsPintar: 1
- }, {
-   lletra: 'asdmwemf we fjaisjd iqwjd',
-   temps: 5,
+ },             
+  {
+   lletra: 'Oh no, oh no...',
+   temps: 47,
    tempsPintar: 1
- }, {
-   lletra: 'asdmwemf we fjaisjd iqwjd',
-   temps: 6,
+ },            
+ {
+   lletra: '¡Oh!',
+   temps: 47,
    tempsPintar: 1
- }, {
-   lletra: 'asdmwemf we fjaisjd iqwjd',
-   temps: 7,
+ },             
+ {
+   lletra: '¡Yeah...!',
+   temps: 47,
    tempsPintar: 1
- }, {
-   lletra: 'asdmwemf we fjaisjd iqwjd',
-   temps: 8,
+ },             
+ {
+   lletra: '¡Dididiri Daddy!',
+   temps: 47,
    tempsPintar: 1
- }, {
-   lletra: 'asdmwemf we fjaisjd iqwjd',
-   temps: 9,
+ },             
+ {
+   lletra: '¡Go!',
+   temps: 47,
    tempsPintar: 1
- }, ];
- var kitipasa;
- var pasa = false;
- var width = 0;
- var musica = document.getElementById('despacito');
- var progressBar = document.getElementById('barTime');
- var time= document.getElementById('timing');
+ },
+ {
+   lletra: '¡Sí, sabes que ya llevo rato mirándote!',
+   temps: 47,
+   tempsPintar: 1
+ },
+ {
+   lletra: '¡Tengo que bailar contigo hoy!',
+   temps: 47,
+   tempsPintar: 1
+ },
+ {
+   lletra: '¡Daddy Yankee!',
+   temps: 47,
+   tempsPintar: 1
+ },
+ {
+   lletra: '¡Vi que tu mirada ya estaba llamándome!',
+   temps: 47,
+   tempsPintar: 1
+ },
+{
+   lletra: '¡Muéstrame el camino que yo voy (Oh)!',
+   temps: 47,
+   tempsPintar: 1
+ },
+ {
+   lletra: '¡Tú, tú eres el imán y yo soy el metal!',
+   temps: 47,
+   tempsPintar: 1
+ },
+ {
+   lletra: '¡Me voy acercando y voy armando el plan!',
+   temps: 47,
+   tempsPintar: 1
+ },
+ {
+   lletra: '¡Solo con pensarlo se acelera el pulso (Oh yeah)!',
+   temps: 47,
+   tempsPintar: 1
+ },
+{
+   lletra: '¡Ya, ya me está gustando más de lo normal!',
+   temps: 47,
+   tempsPintar: 1
+ },
+ {
+   lletra: '¡Todos mis sentidos van pidiendo más!',
+   temps: 47,
+   tempsPintar: 1
+ },
+ {
+   lletra: '¡Fonsi!',
+   temps: 47,
+   tempsPintar: 1
+ },
+ {
+   lletra: '¡Fonsi!',
+   temps: 47,
+   tempsPintar: 1
+ },
+{
+   lletra: '¡Fonsi!',
+   temps: 47,
+   tempsPintar: 1
+ },
+ {
+   lletra: '¡Fonsi!',
+   temps: 47,
+   tempsPintar: 1
+ },
+ {
+   lletra: '¡Fonsi!',
+   temps: 47,
+   tempsPintar: 1
+ },
+ {
+   lletra: '¡Fonsi!',
+   temps: 47,
+   tempsPintar: 1
+ },
+{
+   lletra: '¡Fonsi!',
+   temps: 47,
+   tempsPintar: 1
+ },
+ {
+   lletra: '¡Fonsi!',
+   temps: 47,
+   tempsPintar: 1
+ },
+ {
+   lletra: '¡Fonsi!',
+   temps: 47,
+   tempsPintar: 1
+ },
+ {
+   lletra: '¡Fonsi!',
+   temps: 47,
+   tempsPintar: 1
+ },
+              
+              
+];
+var kitipasa;
+var pasa = false;
+var width = 0;
+var musica = document.getElementById('despacito');
+var progressBar = document.getElementById('barTime');
+var time= document.getElementById('timing');
+var play = document.getElementById('playArrow');
+var restart = document.getElementById('restart');
+var playVideo = document.getElementById('video');
  window.onload = function() {
    
-   kitipasa = function kitipasa() {
+   play.addEventListener("click", function() {
      if(musica.paused){
        musica.play();
-       document.getElementById('playArrow').innerHTML='pause';
+       video.play();
+       document.getElementById('jeje').innerHTML='pause';
        progressBar.setAttribute('max', musica.duration);
      }else{
-        document.getElementById('playArrow').innerHTML='play_arrow';
+        document.getElementById('jeje').innerHTML='play_arrow';
        musica.pause();
+       video.pause();
      }
      pasa=true;
      var phrase = document.getElementById('test');
@@ -69,14 +186,21 @@
        var div = document.getElementById("lletraCanso");
        div.appendChild(p);
      }
-   }
+   })
+   
+    restart.addEventListener("click", function() {
+        musica.currentTime = 0;
+        musica.play();
+        video.play();
+        video.currentTime = 0;
+    })
 
    function animateWidth() {
     if(!musica.paused){
      var inputC = document.getElementById('printLetter');
      width = width + 1;
      inputC.style.width = width + "px";
-     console.log(inputC.style.width);
+     //console.log(inputC.style.width);
      if (pasa == true) {
        width = 0;
        pasa = false;
@@ -88,10 +212,10 @@
    function phraseForward() {
     if(!musica.paused){
      var frases = document.getElementsByClassName("comuna");
-     console.log(frases);
+     //console.log(frases);
      for (var i = 0; i < frases.length; i++) {
        if (i == 0) {
-         console.log(frases[i]);
+        // console.log(frases[i]);
          var phrase = document.getElementById('test');
          var phraseColor = document.getElementById('printLetter');
          phrase.value = frases[i].innerHTML;
@@ -105,10 +229,41 @@
     }
      
    }
-   function colorirBarra(){
+
+   
+   
+    function colorirBarra(){
     if(!musica.paused){
      progressBar.setAttribute('value', musica.currentTime);
-     time.innerHTML='0'+(musica.currentTime/60).toFixed(2)+' - 0'+(musica.duration/60).toFixed(2);
+      var min = 0;
+      var interval = 0;
+      var maxInterval = 60;
+      if(musica.currentTime > maxInterval){
+        maxInterval = maxInterval + 60;
+        interval = interval + 60;
+        min++;
+      }
+     
+      var d = Number(musica.currentTime);
+      var m = Math.floor(d % 3600 / 60);
+      var s = Math.floor(d % 3600 % 60);
+      if(s.length == 2){
+        s = "0" + s.toString();
+      }
+      m = m.toString();
+      s = s.toString();
+      if(m.length < 1){
+        m = "00";
+      }
+      if(m.length == 1){
+        m = "0" + m;
+      }
+      if(s.length < 2){
+        s = "0"+s;
+      }
+      
+      var temps =  m + ":" + s; 
+      time.innerHTML= temps;
     }
    }
    
@@ -116,6 +271,7 @@
       var duration = musica.duration;
       var posCanso = event.offsetX / this.offsetWidth  * duration ;
       musica.currentTime = posCanso;
+      video.currentTime = posCanso;
     });
     //AKI HI PASAREM EL TEMPS DE CANVIAR DE LINIA ☻
     setInterval(phraseForward, 10000);
@@ -123,3 +279,35 @@
     setInterval(animateWidth, 20);
     setInterval(colorirBarra);
  }
+  var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#polina button");
+
+if (window.matchMedia('(prefers-reduced-motion)').matches) {
+    vid.removeAttribute("autoplay");
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+}
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed 
+vid.pause();
+// to capture IE10
+vidFade();
+}); 
+
+
+pauseButton.addEventListener("click", function() {
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+})
